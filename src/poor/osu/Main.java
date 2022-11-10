cbe5e2ab2aee462693a05b7a04df73ea3a694ffb
yutadd <spyk4m49@gmail.com>
2022-11-10 20:46:20 +0900

    add read map from osu file

--- src/poor/osu/Main.java
+++ src/poor/osu/Main.java
@@ -108,50 +108,11 @@ public class Main {
 		Thread th_cursor=new Thread() {
 			@Override
 			public void run() {
-<<<<<<< HEAD
 				while(true) {
 					try {
 						cursor.setBounds(jf.getMousePosition().x-20,jf.getMousePosition().y-30,60,60);
 					}catch(Exception e) {}
 				}
-=======
-				try {
-					Thread.sleep(500);
-					for(int i=0;i<20;i++) {
-						addtg(i*30+100,i*30+100);
-						Thread.sleep(10);
-					}
-					Thread.sleep(10);
-					for(int i=0;i<20;i++) {
-						addtg(i*10+600,(20-i)*30+100);
-						Thread.sleep(10);
-					}
-					for(int i=0;i<20;i++) {
-						addtg(i*-10+900,i*30+100);
-						Thread.sleep(10);
-					}
-					Thread.sleep(10);
-					for(int i=0;i<20;i++) {
-						addtg(i+1000,i*30+100);
-						Thread.sleep(10);
-					}
-					Thread.sleep(50);
-					for(int i=0;i<100;i++) {
-						addtg((int)(Math.sin(i*330)*300)+1400,i*9);
-						Thread.sleep(20);
-					}
-					Thread.sleep(50);
-					for(int i=20;i>0;i--) {
-						addtg((i*60)+800,i*60);
-						Thread.sleep(30);
-					}
-					for(int i=0;i<80;i++) {
-						addtg(i*40,(int)(Math.cos(i*3)*200)+500);
-						Thread.sleep(20);
-					}
-					Thread.sleep(1000);
-				}catch(Exception e) {}
->>>>>>> branch 'main' of https://github.com/yutadd/PoorOsu.git
 			}
 		};
 		th_cursor.start();
@@ -161,7 +122,6 @@ public class Main {
 			try {
 				long currenttime=System.currentTimeMillis();
 				try {
-<<<<<<< HEAD
 					targets.forEach(o->{
 						if(currenttime>=o.expire) {
 							o.eliminate();
@@ -182,12 +142,6 @@ public class Main {
 				precious.setText((int)((double)((double)hited/(double)eliminated_target)*100.0)+"% "+hited+"/"+eliminated_target);
 			}catch(Exception e) {e.printStackTrace();}
 			jf.repaint();
-=======
-				precious.setText((int)((double)((double)hited/(double)eliminated_target)*100.0)+"% "+hited+"/"+eliminated_target);
-				}catch(Exception e) {}
-				cursor.setBounds(jf.getMousePosition().x-20,jf.getMousePosition().y-30,60,60);}catch(Exception e) {}
-				jf.repaint();
->>>>>>> branch 'main' of https://github.com/yutadd/PoorOsu.git
 			//Thread.sleep(6);
 		}
 	}
